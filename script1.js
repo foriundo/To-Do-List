28616449c30748c4ebfe68d1beb1e9f9347cb45f
Fernando Oriundo <faoriundo@gmail.com>
2024-05-02 22:32:16 -0400

    small error update

--- script1.js
+++ script1.js
@@ -1,7 +1,8 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+
 function addTask(){
-    if(inputBox.value === ''){
+    if(inputBox.value ===''){
         alert("You must write something!");
     }
     else{
